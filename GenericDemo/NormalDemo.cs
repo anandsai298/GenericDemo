a7882ee539405c79fc665900f7aac2177852539d
anandsaikumar <anandsai298@gmail.com>
2023-03-20 18:44:21 +0530

    [sai] Add : created generic method byb using datatypes int double char

---
+++ GenericDemo/NormalDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class NormalDemo
+    {
+        //without generic
+        //method overloading same method name with diff parameters
+        public void print(int[] arr)
+        {
+            foreach(var data in arr) 
+            { 
+            Console.WriteLine(data);
+            }
+        }
+        public void print(double[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        public void print(char[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace GenericDemo
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            int[] intArr = { 1, 2, 3, 4, 5 };
+            double[] doubleArr = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArr = { 'a', 'b', 'c', 'd' };
+            Console.WriteLine("without generic method");
+            NormalDemo normalDemo = new NormalDemo();
+            normalDemo.print(intArr);
+            normalDemo.print(doubleArr);
+            normalDemo.print(charArr);
+
+            Console.WriteLine("with generic method");
+            GenericMethod genericMethod = new GenericMethod();
+            genericMethod.Print<int>(intArr);
+            genericMethod.Print<double>(doubleArr);
+            genericMethod.Print<char>(charArr);
+        }
+    }
+}
