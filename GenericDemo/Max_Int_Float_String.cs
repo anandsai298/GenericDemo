3f5b2841a58d1b69b94f92ee11a1f9bb341711cb
anandsaikumar <anandsai298@gmail.com>
2023-03-20 19:15:03 +0530

    [sai] Add : created Max values of in an d float and strings

---
+++ GenericDemo/Max_Int_Float_String.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class Max_Int_Float_String
+    {
+        public void MaxIntValue(int a, int b,int c)
+        {
+            if (a > b && a > c)
+                Console.WriteLine(a + " is max int value");
+            if (b > a && b > c)
+                Console.WriteLine(b + " is max int value");
+            else
+                Console.WriteLine(c + "is max int value");
+        }
+        public void MaxFloatValue(float a, float b, float c)
+        {
+            if (a > b && a > c)
+                Console.WriteLine(a + " is max float value");
+            if (b > a && b > c)
+                Console.WriteLine(b + " is max float value");
+            else
+                Console.WriteLine(c + "is max float value");
+        }
+        public void MaxStringValue(string a, string b, string c)
+        {
+            if (a.CompareTo(b)>0 && a.CompareTo(c)>0)
+                Console.WriteLine(a + " is max string value");
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                Console.WriteLine(b + " is max string value");
+            else
+                Console.WriteLine(c + "is max string value");
+        }
+    }
+}
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -9,6 +9,7 @@ namespace GenericDemo
             int[] intArr = { 1, 2, 3, 4, 5 };
             double[] doubleArr = { 1.1, 2.2, 3.3, 4.4 };
             char[] charArr = { 'a', 'b', 'c', 'd' };
+            float[] Float = { 1.5f, 2.5f, 3.5f };
             Console.WriteLine("without generic method");
             NormalDemo normalDemo = new NormalDemo();
             normalDemo.print(intArr);
@@ -20,6 +21,12 @@ namespace GenericDemo
             genericMethod.Print<int>(intArr);
             genericMethod.Print<double>(doubleArr);
             genericMethod.Print<char>(charArr);
+            genericMethod.Print<float>(Float);
+
+            Max_Int_Float_String max= new Max_Int_Float_String();
+            max.MaxIntValue(10,20,30);
+            max.MaxFloatValue(10.5f,24.6f,25.0f);
+            max.MaxStringValue("ask","sai","anand");
         }
     }
 }
