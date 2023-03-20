9875470e5c6fa35565fa906ef0294c1deb327dea
anandsaikumar <anandsai298@gmail.com>
2023-03-20 19:28:41 +0530

    [sai] Add : Adding Generic method to findmax value

--- GenericDemo/GenericMethod.cs
+++ GenericDemo/GenericMethod.cs
@@ -15,5 +15,15 @@ namespace GenericDemo
                 Console.WriteLine(data);
             }
         }
+        //Generic method
+        public void FindMax<T>(T a, T b,T c) where T : IComparable//Icomparable is inbuilt to have the compareto method
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                Console.WriteLine(a + " is max string value");
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                Console.WriteLine(b + " is max string value");
+            else
+                Console.WriteLine(c + "is max string value");
+        }
     }
 }
